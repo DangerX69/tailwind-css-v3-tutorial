f069b6eddefd83f44b8dc1faedd39a64d92b2ac9
Biswash Khanal <biswashkhanal67@gmail.com>
2025-05-04 22:33:24 +0545

    even-more-setups

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
-  return (
-    <div>
-      <div className="container bg-red-500 mx-auto">
-        <h1>HelloTailwindCSS</h1>
-      </div>
-    </div>
-  )
-}
+	return (
+		<main className="relative">
+			Nav
+			<section className="xl:sm:px-16 px-8 sm:py-24 py-12 wide:sm:pr-16 pr-8 sm:pb-24 pb-12">Hero</section>
+			<section className="p">Popular Products</section>
+			<section className="p">SuperQuality</section>
+			<section className="p">Services</section>
+			<section className="p">Special Offer</section>
+		</main>
+	);
+};
 
-export default App
+export default App;
