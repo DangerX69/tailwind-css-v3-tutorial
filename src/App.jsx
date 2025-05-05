2324663419e77f9ac52304394a7ea591b4c8d238
Biswash Khanal <biswashkhanal67@gmail.com>
2025-05-05 22:14:02 +0545

    reverted to tailwindv3 for ease of learning

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ const App = () => {
 	return (
 		<main className="relative">
 			Nav
-			<section className="xl:sm:px-16 px-8 sm:py-24 py-12 wide:sm:pr-16 pr-8 sm:pb-24 pb-12">Hero</section>
+			<section className="xl:padding-l ">Hero</section>
 			<section className="p">Popular Products</section>
 			<section className="p">SuperQuality</section>
 			<section className="p">Services</section>
