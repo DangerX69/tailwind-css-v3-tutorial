fca0322dec1b09eaef714980acaec24d93306eb1
Biswash Khanal <biswashkhanal67@gmail.com>
2025-05-07 03:36:53 +0545

    Hero-section 80% done

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -6,14 +6,14 @@ import { navLinks } from "../Constants";
 
 const NavBar = () => {
 	return (
-		<header className="padding-x py-10 z-10 w-full sticky top-0">
+		<header className="padding-x py-10 z-10 w-full sticky top-0 bg-slate-50 opacity-100 rounded-full">
 			<nav className="flex justify-between items-center max-container">
 				<a href="/">
 					<img
 						src={headerLogo}
 						alt="Nike Logo"
-                        width={130}
-                        height={29}
+						width={130}
+						height={29}
 					/>
 				</a>
 				<ul className="flex-1 flex justify-end items-center gap-10 max-md:hidden">
@@ -30,9 +30,14 @@ const NavBar = () => {
 						);
 					})}
 				</ul>
-                <div className="md:hidden">
-                    <img src={hamburger} alt="hamburger menu" width={25} height={25} />
-                </div>
+				<div className="md:hidden">
+					<img
+						src={hamburger}
+						alt="hamburger menu"
+						width={25}
+						height={25}
+					/>
+				</div>
 			</nav>
 		</header>
 	);
